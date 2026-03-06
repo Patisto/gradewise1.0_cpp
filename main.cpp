45d432ab728fd8fd7bd9d5e84db486996275a602
Patrick Solomon <bsc-inf-08-24@unima.ac.mw>
2026-03-06 04:20:23 +0200

    Add program structure

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+// Structure to hold component data (Tests, Assignments, etc.)
+struct Component {
+    string name;
+    double weight;
+    double score;
+    double maxScore;
+    double contribution;
+};
+int main(){
+    return 0;
+}
